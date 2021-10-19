a97960ac6a2e412fa1f12c05ea37a59d65c67953
Burak GÜLER <burak.guler@isnetservis.com.tr>
2021-10-19 18:03:34 +0300

    spliting the components

--- xpenses-react-app/src/components/ExpenseItem.js
+++ xpenses-react-app/src/components/ExpenseItem.js
@@ -1,13 +1,11 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const expenseDate = new Date(2021, 10, 19);
-  const expenseTitle = "House Rent";
-  const expenseAmount = 2500.0;
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
