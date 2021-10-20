33567340d643f397dc02f4284acec7e49582b78c
Burak GÜLER <burak.guler@isnetservis.com.tr>
2021-10-20 16:24:40 +0300

    changing functions to arrow functions

--- xpenses-react-app/src/App.js
+++ xpenses-react-app/src/App.js
@@ -1,28 +1,30 @@
-import Expenses from './components/Expenses/Expenses';
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
-      id: 'e1',
-      title: 'Tuvalet Kağıdı',
+      id: "e1",
+      title: "Tuvalet Kağıdı",
       amount: 94.12,
       date: new Date(2021, 8, 14),
     },
-    { id: 'e2', 
-      title: 'French Press Kahve', 
-      amount: 27.99, 
-      date: new Date(2021, 9, 21) },
     {
-      id: 'e3',
-      title: 'Ayakkabılık',
-      amount: 50.00,
-      date: new Date(2021, 9,21),
+      id: "e2",
+      title: "French Press Kahve",
+      amount: 27.99,
+      date: new Date(2021, 9, 21),
     },
     {
-      id: 'e4',
-      title: 'Ev Kirası',
-      amount: 3010.00,
-    
+      id: "e3",
+      title: "Ayakkabılık",
+      amount: 50.0,
+      date: new Date(2021, 9, 21),
+    },
+    {
+      id: "e4",
+      title: "Ev Kirası",
+      amount: 3010.0,
+
       date: new Date(2021, 10, 10),
     },
   ];
@@ -32,6 +34,6 @@ function App() {
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
